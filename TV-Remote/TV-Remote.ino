11fe9c78600bd914795eae9af060df90b2f198e3
KabrG <kabirguron@gmail.com>
2024-06-24 01:45:00 -0400

    fixed bugs

--- TV-Remote/TV-Remote.ino
+++ TV-Remote/TV-Remote.ino
@@ -1,8 +1,8 @@
 /*
- *  Credit to Armin Joachimsmeyer for the SimpleSender examplecode + Library support
- *  Copyright (C) 2020-2022  Armin Joachimsmeyer
- *  armin.joachimsmeyer@gmail.com
-  */
+    Credit to Armin Joachimsmeyer for the SimpleSender examplecode + Library support
+    Copyright (C) 2020-2022  Armin Joachimsmeyer
+    armin.joachimsmeyer@gmail.com
+*/
 #include <Arduino.h>
 
 #if !defined(ARDUINO_ESP32C3_DEV)
@@ -17,35 +17,42 @@
 const int on_pin = 6;
 const int volume_up_pin = 8;
 const int volume_down_pin = 9;
-const int record_input_pin = 10;
-const int repeat_input_pin = 12;
+const int change_tv_pin = 10;
+
+const int num_tv = 2;
+
+// Global TV counter
+int tv_mode = 0;
+
+//const int record_input_pin = 10;
+//const int repeat_input_pin = 12;
 
 struct command_controls {
   const uint8_t s_address;
   const uint16_t s_command;
   const uint8_t s_repeat;
-  
+
 };
 
 
 
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 
-    Serial.begin(115200);
-    
-    pinMode(on_pin, INPUT);
-    pinMode(volume_up_pin, INPUT);
-    pinMode(volume_down_pin, INPUT);
-//    pinMode(change_tv, INPUT);
+  Serial.begin(115200);
 
-    // Just to know which program is running on my Arduino
-    Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
-    Serial.print(F("Send IR signals at pin "));
-    Serial.println(IR_SEND_PIN);
+  pinMode(on_pin, INPUT);
+  pinMode(volume_up_pin, INPUT);
+  pinMode(volume_down_pin, INPUT);
+  //    pinMode(change_tv, INPUT);
 
-    IrSender.begin(); // Start with IR_SEND_PIN -which is defined in PinDefinitionsAndMore.h- as send pin and enable feedback LED at default feedback LED pin
-    disableLEDFeedback(); // Disable feedback LED at default feedback LED pin
+  // Just to know which program is running on my Arduino
+  Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
+  Serial.print(F("Send IR signals at pin "));
+  Serial.println(IR_SEND_PIN);
+
+  IrSender.begin(); // Start with IR_SEND_PIN -which is defined in PinDefinitionsAndMore.h- as send pin and enable feedback LED at default feedback LED pin
+  disableLEDFeedback(); // Disable feedback LED at default feedback LED pin
 
 }
 
@@ -54,8 +61,8 @@ void setup() {
 //const uint16_t sonyCommandPowerOn = 0x15; // 7-bit command for power on
 //const uint8_t sRepeats = 3;
 
-// 0x01 0x12 1 gives volume up 
-// 0x01 0x13 1 gives volume down 
+// 0x01 0x12 1 gives volume up
+// 0x01 0x13 1 gives volume down
 
 // 0x01 0x0 3 gives the number 1
 // 0x01 0x1 3 gives the number 2
@@ -73,55 +80,76 @@ void sony_control(void) {
   int on_button = digitalRead(on_pin);
   int volume_up_button = digitalRead(volume_up_pin);
   int volume_down_button = digitalRead(volume_down_pin);
-
+  
+  // s_address (uint8_t), s_command (uint16_t), s_repeat (uint8_t) for SONY
   struct command_controls on = {0x01, 0x15, 3};
   struct command_controls volume_up = {0x01, 0x12, 3};
   struct command_controls volume_down = {0x01, 0x13, 3};
 
-    // If button is pressed send power code command
-   if (on_button == HIGH) {
-      // Send the Sony power on command
-      IrSender.sendSony(on.s_address, on.s_command, on.s_repeat);
-   }
-   else if (volume_up_button == HIGH) {
-      IrSender.sendSony(volume_up.s_address, volume_up.s_command, volume_up.s_repeat);
-   }
-   else if (volume_down_button == HIGH) {
-      IrSender.sendSony(volume_down.s_address, volume_down.s_command, volume_down.s_repeat);
-   }
+  // If button is pressed send power code command
+  if (on_button == HIGH) {
+    // Send the Sony power on command
+    IrSender.sendSony(on.s_address, on.s_command, on.s_repeat);
+  }
+  else if (volume_up_button == HIGH) {
+    IrSender.sendSony(volume_up.s_address, volume_up.s_command, volume_up.s_repeat);
+  }
 
-  
+  else if (volume_down_button == HIGH) {
+    IrSender.sendSony(volume_down.s_address, volume_down.s_command, volume_down.s_repeat);
+  }
+
+}
+
+void samsung_control() {
+  int on_button = digitalRead(on_pin);
+  int volume_up_button = digitalRead(volume_up_pin);
+  int volume_down_button = digitalRead(volume_down_pin);
+
+  struct command_controls on = {0x01, 0x15, 3}; // Configure for true values
+  struct command_controls volume_up = {0x01, 0x12, 3};
+  struct command_controls volume_down = {0x01, 0x13, 3};
+
+  // If button is pressed send power code command
+  if (on_button == HIGH) {
+    // Send the Sony power on command
+    IrSender.sendSamsung(on.s_address, on.s_command, on.s_repeat);
+  }
+  else if (volume_up_button == HIGH) {
+    IrSender.sendSamsung(volume_up.s_address, volume_up.s_command, volume_up.s_repeat);
+  }
+
+  else if (volume_down_button == HIGH) {
+    IrSender.sendSamsung(volume_down.s_address, volume_down.s_command, volume_down.s_repeat);
+  }
+}
+
+void change_tv_mode() {
+  ++tv_mode;
+  if (tv_mode > num_tv) {
+    tv_mode = 0;
+  }
+  delay(1000);
+
 }
 
 
-void record_ir() {
-    int record_button = digitalRead(record_input_pin);
-    int repeat_input_button = digitalRead(repeat_input_pin);
-
-    if (record_button == HIGH) {
-      
-    }
-    else if (repeat_input_button == HIGH) {
-      
-    }
-  
-}
 
 void loop() {
 
-    record_ir();
-    sony_control();
-  // If button is pressed send power code command
-//   if (on_button == HIGH) {
-//      // Send the Sony power on command
-//      IrSender.sendSony(sonyAddress, sonyCommandPowerOn, sRepeats);
-//   }
-//   else if (volume_up_pin == HIGH) {
-//    
-//   }
+  if (digitalRead(change_tv_pin) == HIGH) {
+    change_tv_mode();
+  }
 
-    // Wait for a while before sending the command again
-    delay(100); // second delay
+  if (tv_mode == 0) {
+    sony_control();
+  }
+  else if (tv_mode == 12) {
+    samsung_control();
+  }
+
+  // Wait for a while before sending the command again
+  delay(100); // second delay
 }
 
 
